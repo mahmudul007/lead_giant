cf965efef7cdecc5427534b1b7f9ab2561418328
mahmudul007 <sayedmahmudul05@gmail.com>
2023-12-12 13:45:14 +0600

    push

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -51,83 +51,8 @@
  ============================================= -->
     <div id="wrapper" class="clearfix">
 
-        {{-- <!-- Header ============================================= -->
-        <header id="header" class="full-header transparent-header header-size-custom" data-sticky-shrink="false"
-            data-mobile-sticky="true">
-            <div id="header-wrap">
-                <div class="container">
-                    <div class="header-row">
+      
 
-                        <!-- Logo
-                                 ============================================= -->
-                        <div id="logo" class="me-lg-3">
-                            <a href="{{route('frontend.index')}}"  class="standard-logo" data-dark-logo="spm.png"><img src={{asset("spm.png")}}
-                                    alt="Lead Giant Logo"></a>
-                            <a href="{{route('frontend.index')}}"  class="retina-logo" data-dark-logo="spm.png"><img src={{asset("spm.png")}}
-                                    alt="Lead Giant Logo"></a>
-                        </div><!-- #logo end -->
-
-                        <div id="primary-menu-trigger">
-                            <svg class="svg-trigger" viewBox="0 0 100 100">
-                                <path
-                                    d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20">
-                                </path>
-                                <path d="m 30,50 h 40"></path>
-                                <path
-                                    d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20">
-                                </path>
-                            </svg>
-                        </div>
-
-                        <!-- Primary Navigation
-                                          ============================================= -->
-                        <nav class="primary-menu">
-
-                            <ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250"
-                                data-offset="65">
-                                <li class="menu-item">
-                                    <a href="{{route('frontend.index')}}"  class="menu-link fs-6 fw-bolder" >
-                                        <div>Home</div>
-                                    </a>
-                                </li>
-
-                                <li class="menu-item">
-                                    <a href="{{route('property.index')}}" class="menu-link  fs-6 fw-bolder" >
-                                        <div>blogs</div>
-                                    </a>
-                                </li>
-
-                                <li class="menu-item">
-                                    <a href="{{route('service.index')}}"  class="menu-link  fs-6 fw-bolder"
-                                       >
-                                        <div>brands</div>
-                                    </a>
-                                </li>
-
-                                <li class="menu-item">
-                                    <a href="{{route('frontend.contact')}}" class="menu-link  fs-6 fw-bolder"
-                                      >
-                                        <div>Contact</div>
-                                    </a>
-                                </li>
-                                <li class="menu-item">
-                                    <a href="{{route('frontend.about')}}"  class="menu-link  fs-6 fw-bolder"
-                                      >
-                                        <div>About</div>
-                                    </a>
-                                </li>
-                            </ul>
-
-                        </nav><!-- #primary-menu end -->
-
-                    </div>
-                </div>
-            </div>
-            <div class="header-wrap-clone"></div>
-        </header><!-- #header end --> --}}
-
-        <!-- Top Bar
-  ============================================= -->
         <div id="top-bar" class="transparent-topbar">
             <div class="container clearfix">
 
@@ -153,8 +78,7 @@
 
                     <div class="col-12 col-md-auto dark">
 
-                        <!-- Top Social
-      ============================================= -->
+                        <!-- Top Social ============================================= -->
                         <ul id="top-social">
                             <li><a href="https://facebook.com/semicolonweb" class="si-facebook" target="_blank"><span
                                         class="ts-icon"><i class="icon-facebook"></i></span><span
@@ -189,9 +113,11 @@
 
                         <!-- Logo    ============================================= -->
                         <div id="logo">
-                            <a href="/" class="standard-logo" data-dark-logo="fahad_logo.png"><img
+                            <a href="/" class="standard-logo" data-dark-logo="fahad_logo.png">
+                                <img style="height: 50px;widht:75px"
                                     src="{{asset('fahad_logo.png')}}" alt="Lead giant Logo"></a>
-                            <a href="/" class="retina-logo" data-dark-logo="lfahad_logo.png"><img
+                            <a href="/" class="retina-logo" data-dark-logo="lfahad_logo.png">
+                                <img style="height: 50px;widht:75px"
                                 src="{{asset('fahad_logo.png')}}" alt="Lead giant Logo"></a>
                         </div><!-- #logo end -->
 
